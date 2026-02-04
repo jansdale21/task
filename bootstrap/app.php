0817c67710b733a04d91ffe3878281c685882cf7
Lance Quan <sokarisatlasia@gmail.com>
2026-02-04 17:39:36 +0800

    new changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
-        'tasks/*',
+        'tasks/*','authors/*','descriptions/*','work-items/*'
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
